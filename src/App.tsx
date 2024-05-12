53f00260e81f0f5882daf041af081ff43df7c8c3
kaseampongtho@gosoft.co.th <kaseampongtho@gosoft.co.th>
2024-05-12 21:57:09 +0700

    add  content.json , fix line error, create content-manager file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Fade from './component/fade';
 import Image from './component/image';
 import MenuList from './component/menu-list';
+import ContentManager from './component/content-manager';
 
 const App = () => {
   const [action, setAction] = useState<string | undefined>(undefined);
@@ -13,8 +14,8 @@ const App = () => {
     [],
   );
   return (
-    <div className="flex flex-row w-100  gap-20">
-      <div className="flex w-1/3 text-start flex-col">
+    <div className="flex flex-row w-100">
+      <div className="flex w-1/3 text-start flex-col static">
         <Fade delayCounter={1500}>
           <Image url="../src/assets/me.png" />
           <MenuList
@@ -23,16 +24,18 @@ const App = () => {
           />
         </Fade>
       </div>
-      <div className="flex flex-col w-2/3 pr-10">
+      <div className="flex flex-col w-2/3">
         <div className="flex flex-col w-70">
           <Fade delayCounter={1000}>
-            <div className="text-start text-5xl gap-10">
+            <div className="text-end text-5xl">
               <span>KASEAMPONG</span>
               <span className="ml-14">THONGPASART</span>
             </div>
-
-            <h1 className="text-end pr-7"> Developer </h1>
+            <div className="text-end text-5xl mt-5">
+              FULL - STACK DEVELOPER
+            </div>
           </Fade>
+          <ContentManager></ContentManager>
         </div>
       </div>
     </div>
---
+++ src/component/content-manager.tsx
@@ -0,0 +1,18 @@
+import { IMenu, Menu } from '../interface/action-menu';
+
+interface props {
+  action?: string;
+}
+
+const ContentManager = ({ action }: props) => {
+  //   const ListMenu: IMenu[] = [
+  //     { name: Menu.ABOUT, display: 'About' },
+  //     { name: Menu.EDUCATION, display: 'Education' },
+  //     { name: Menu.WORK_EXPERIENCE, display: 'Work Experience' },
+  //     { name: Menu.CONTACT, display: 'Contact' },
+  //   ];
+
+  return <></>;
+};
+
+export default ContentManager;
